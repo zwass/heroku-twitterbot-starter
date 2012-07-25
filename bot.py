96ce676d9bb4cbdd0cc73b327f493e597662476f
Zachary Wasserman <zachwass2000@gmail.com>
2012-07-24 22:36:30 -0700

    initial commit

---
+++ bot.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+import tweepy
+
+class TwitterAPI:
+    """
+    Class for accessing the Twitter API.
+
+    Requires API credentials to be available in environment
+    variables. These will be set appropriately if the bot was created
+    with init.sh included with the heroku-twitterbot-starter
+    """
+    def __init__(self):
+        consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
+        consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+        access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+        auth.set_access_token(access_token, access_token_secret)
+        self.api = tweepy.API(auth)
+
+    def tweet(self, message):
+        """Send a tweet"""
+        self.api.update_status(message)
+
+if __name__ == "__main__":
+    twitter = TwitterAPI()
+    twitter.tweet("Hello world!") #You probably want to remove this line
+    while True:
+        #Send a tweet here!
+        time.sleep(60)
---
+++ init.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#This script sets up Heroku + your local environment for Twitter
+
+echo "First, let's create a Heroku app for this bot."
+echo -n "What shall we call this thing, then? "
+read botname
+
+while ! heroku create --stack cedar $botname; do
+    echo "We failed to create the Heroku app"
+    echo -n "How about we try another name? "
+    read botname
+done
+#Heroku app has now been successfully created
+
+echo
+echo "We're going to need some Twitter API credentials."
+echo "Check out https://dev.twitter.com/docs/auth/tokens-devtwittercom"
+echo "for instructions on creating your Consumer Key and Access Token."
+echo "Be sure to set to Access Level to \"Read and write\" in the Settings tab."
+echo
+
+echo -n "Consumer key: "
+read consumerkey
+echo -n "Consumer secret: "
+read consumersecret
+echo -n "Access token: "
+read accesstoken
+echo -n "Access token secret: "
+read accesstokensecret
+
+#add the twitter credentials to the Heroku app environment
+echo
+echo "Sending your Twitter API credentials up to Heroku..."
+heroku config:add TWITTER_CONSUMER_KEY=$consumerkey \
+    TWITTER_CONSUMER_SECRET=$consumersecret \
+    TWITTER_ACCESS_TOKEN=$accesstoken \
+    TWITTER_ACCESS_TOKEN_SECRET=$accesstokensecret
+
+#create a script for setting up your local environment
+cat <<EOF > setup_env.sh
+export TWITTER_CONSUMER_KEY=$consumerkey
+export TWITTER_CONSUMER_SECRET=$consumersecret
+export TWITTER_ACCESS_TOKEN=$accesstoken
+export TWITTER_ACCESS_TOKEN_SECRET=$accesstokensecret
+EOF
+
+echo
+echo "Pushing to Heroku so that we can set up worker process"
+git push heroku master
+#Now Heroku should recognize worker when we try to scale
+
+echo
+echo "Scaling to web=0 worker=1"
+heroku ps:scale web=0 worker=1
+
+echo "Now you can \"source setup_env.sh\" and get to work on that sweet bot!"
